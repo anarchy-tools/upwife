ca5f6685ddbe60ff0c9ee6e0c465a9a4f9104ef0
anarchy-tools <anarchytools@gmail.com>
2022-08-27 17:35:09 -0500

    add logging

--- upwife-hinge.sh
+++ upwife-hinge.sh
@@ -4,27 +4,36 @@ random_number() {
   WAIT_TIME=$[ $RANDOM % 5 + 1 ]
 }
 
+timestamp() {
+	echo -n $(echo upwife)\|$(date +"%T")\| # current time
+}
+
+log() {
+  timestamp
+  echo $@
+}
+
 target_potential_wife() {
-  echo targeting potential wife
+  log targeting potential wife
   adb shell input tap 639 873
   adb shell input tap 639 950
   adb shell input tap 639 980
-  echo target acquired
+  log target acquired
 }
 
 load_wife_retrieval_payload() {
-  echo loading wife retrieval payload
+  log loading wife retrieval payload
   sleep 3
 }
 
 send_wife_retrieval_payload() {
-  echo sending wife retrieval payload
+  log sending wife retrieval payload
   adb shell input tap 466 1237
-  echo success
+  log success
 } 
 
 prepare_for_next_potential_wife() {
-  echo preparing for next wife
+  log preparing for next wife
   random_number
   sleep $WAIT_TIME
 }
